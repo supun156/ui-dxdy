b3d79d2c229900dd50e12062a20e1e6c0da7a818
Supun Lakmal <50650071+supun156@users.noreply.github.com>
2025-04-13 12:08:12 +0530

    🎨

---
+++ dist/Button.d.ts
@@ -0,0 +1,7 @@
+import React, { ReactNode, ButtonHTMLAttributes } from "react";
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children: ReactNode;
+}
+declare const Button: React.FC<ButtonProps>;
+export default Button;
+//# sourceMappingURL=Button.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Button = void 0;
+// Re-export the Button component
+var Button_1 = require("./Button");
+Object.defineProperty(exports, "Button", { enumerable: true, get: function () { return __importDefault(Button_1).default; } });
+// If you add more components later, export them here too:
+// export { default as Card } from './Card';
+// export { default as Modal } from './Modal';
+//# sourceMappingURL=index.js.map
